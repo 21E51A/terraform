1058e9df00efcb4723455cce31fc1367f1a3f657
21E51A <akashpaiavula2003@gmail.com>
2025-11-15 18:40:08 +0530

    day-8-child-module

---
+++ Day-8 child-module/variable.tf
@@ -0,0 +1,10 @@
+variable "cidr_block" {}
+variable "subnet_1_cidr" {}
+variable "subnet_2_cidr" {}
+variable "az1" {}
+variable "az2" {}
+
+variable "ami_id" {}
+variable "instance_type" {}
+variable "subnet_1_id" {}   # not needed if you use module output
+variable "db_password" {}
